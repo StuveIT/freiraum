99123d7e4fc1855aacae79f93d2c4064291e7fdc
Anton Pogrebnjak <contact@pantonius.dev>
2025-06-03 15:09:13 +0200

    Humble beginnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://freiraum.stuve-uni-kn.de',
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex]
+  },
+
+  integrations: [sitemap(), mdx()]
+});
---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Freiraum';
+export const SITE_DESCRIPTION = 'Ein einfaches Tool, um sich schnell freie Räume an der Universität Konstanz aufzufinden -- funktioniert nur im Uni-Netz';
