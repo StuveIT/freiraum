02e0162b5bde425a3f096fba0af1a751d8a86b12
Anton Pogrebnjak <contact@pantonius.dev>
2026-07-03 18:50:57 +0200

    Adapted site description

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Freiraum';
-export const SITE_DESCRIPTION = 'Ein einfaches Tool, um sich schnell freie Räume an der Universität Konstanz aufzufinden -- funktioniert nur im Uni-Netz';
+export const SITE_DESCRIPTION = 'Du findest in der Bibliothek keine freien Arbeitsplätze mehr? Kein Problem: Du kannst auch einfach freie Seminarräume zum Lernen verwenden. Um herauszufinden, welche gerade frei sind, hat die StuVe das Freiraum-Tool entwickelt. Dieses greift auf die Raumverwaltungssoftware der Uni zu und zeigt die Verfügbarkeit von Räumen übersichtlich an. Du kannst die Verfügbarkeit zum aktuellen Zeitpunkt sehen und auch Uhrzeit und Datum auswählen. Mit Klick auf den Raum bekommst du weitere Infos zur Kapazität und Ausstattung. Wichtig: Du musst im Uni-Netzwerk sein, um auf die Daten zuzugreifen (wenn du zuhause bist, kannst du den VPN benutzen).';
