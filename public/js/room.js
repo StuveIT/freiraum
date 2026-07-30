3bffcd268a4321793382aa72f02e85bd4c4a7232
Anton Pogrebnjak <contact@pantonius.dev>
2026-07-30 14:06:10 +0200

    Proper

--- public/js/room.js
+++ public/js/room.js
@@ -13,7 +13,7 @@ export class Room {
     this.comments = comments;
     this.number_of_seats = number_of_seats;
     this.room_type = room_type;
-    this.open = true;
+    this.open = PERIOD_BREAK_WHITELIST.includes(name);
   }
 }
 
@@ -48,7 +48,7 @@ export async function fetchAllRooms() {
   const regular_xml = await fetchXML("http://134.34.26.182/TestRRM/WcfDataService.svc/GetAllRooms()?$orderby%20=%20Raum", map_to_room);
   const bib_xml = await fetchXML("http://134.34.26.182/roomresbib/WcfDataService.svc/GetAllRooms()?$orderby%20=%20Raum", map_to_room);
 
-  let overall_xml = regular_xml.concat(bib_xml).sort((a, b) => a.name.localeCompare(b.name)).filter((room) => !BLACKLIST.includes(room.name) && PERIOD_BREAK_WHITELIST.includes(room.name));
+  let overall_xml = regular_xml.concat(bib_xml).sort((a, b) => a.name.localeCompare(b.name)).filter((room) => !BLACKLIST.includes(room.name));
 
   return overall_xml;
 }
